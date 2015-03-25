019f734e608d7ca3532ae613b4b5f5eaa21c80b8
Jens Malmborg <jnsmalm@gmail.com>
2015-03-25 21:04:14 +0100

    Updated mouse example

--- examples/input-mouse.js
+++ examples/input-mouse.js
@@ -1,15 +1,15 @@
 var game = ko.import("../lib/game.js");
 
 game.init();
-var window = game.window;
 
 game.update = function () {
-  window.setTitle("x: " + window.mouse.x + ", y: " + window.mouse.y);
-  if (window.mouse.isButtonDown(0)) {
-    window.setTitle("0");
+  var mouse = game.window.mouse;
+  game.window.setTitle("x: " + mouse.x + ", y: " + mouse.y);
+  if (mouse.isButtonDown(0)) {
+    game.window.setTitle("0");
   }
-  if (window.mouse.isButtonDown(1)) {
-    window.setTitle("1");
+  if (mouse.isButtonDown(1)) {
+    game.window.setTitle("1");
   }
 };
 

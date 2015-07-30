4b832592fd4ab2871e1b2f7a207a5f58d9be4712
Jens Malmborg <jnsmalm@gmail.com>
2015-07-30 21:14:09 +0200

    Updated to reflect changes in API.

--- examples/create-window.js
+++ examples/create-window.js
@@ -1,11 +1,17 @@
 var window = new Window({
+  // Set window width to 1024.
   width: 1024,
+  // Set window height to 576.
   height: 576,
+  // Window should not be full screen.
   fullscreen: false,
 });
 
 while (!window.isClosing()) {
-  window.clear();
-  window.swapBuffers();
+  // Clear window graphics to black.
+  window.graphics.clear({r:0, g:0, b:0, a:1});
+  // Present the back buffer.
+  window.graphics.present();
+  // Poll and handle window events.
   window.pollEvents();
 }

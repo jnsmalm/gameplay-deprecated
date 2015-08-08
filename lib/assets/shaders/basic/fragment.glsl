6c183878923cf7f3ad3918b3a875d1c67619fe4b
Jens Malmborg <jnsmalm@gmail.com>
2015-08-08 22:41:35 +0200

    Added lighting to basic shader.

--- lib/assets/shaders/basic/fragment.glsl
+++ lib/assets/shaders/basic/fragment.glsl
@@ -1,19 +1,102 @@
-#version 150 core
+/* Copyright 2012 Thomas Dalling
 
-in vec3 f_position;
-in vec4 f_color;
-in vec2 f_texcoords;
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-out vec4 outColor;
+   http://www.apache.org/licenses/LICENSE-2.0
 
-uniform sampler2D texture0;
-uniform int textureEnabled;
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
 
-void main()
-{
-    if (textureEnabled != 0) {
-        outColor = texture(texture0, f_texcoords) * f_color;
+#version 150
+
+uniform mat4 model;
+uniform vec3 cameraPosition = vec3(0,0,0);
+
+uniform sampler2D materialTex;
+uniform float materialShininess;
+uniform vec3 materialSpecularColor;
+uniform vec3 materialColor;
+
+#define MAX_LIGHTS 10
+uniform int numLights;
+uniform struct Light {
+   vec4 position;
+   vec3 intensities; //a.k.a the color of the light
+   float attenuation;
+   float ambientCoefficient;
+   float coneAngle;
+   vec3 coneDirection;
+} allLights[MAX_LIGHTS];
+
+in vec2 fragTexCoord;
+in vec3 fragNormal;
+in vec3 fragVert;
+
+out vec4 finalColor;
+
+vec3 ApplyLight(Light light, vec3 surfaceColor, vec3 normal, vec3 surfacePos,
+    vec3 surfaceToCamera) {
+
+    vec3 surfaceToLight;
+    float attenuation = 1.0;
+    if(light.position.w == 0.0) {
+        //directional light
+        surfaceToLight = normalize(light.position.xyz);
+        attenuation = 1.0; //no attenuation for directional lights
     } else {
-        outColor = f_color;
+        //point light
+        surfaceToLight = normalize(light.position.xyz - surfacePos);
+        float distanceToLight = length(light.position.xyz - surfacePos);
+        attenuation = 1.0 / (1.0 + light.attenuation * pow(distanceToLight, 2));
+
+        //cone restrictions (affects attenuation)
+        float lightToSurfaceAngle = degrees(acos(dot(-surfaceToLight,
+            normalize(light.coneDirection))));
+        if(lightToSurfaceAngle > light.coneAngle){
+            attenuation = 0.0;
+        }
     }
+
+    //ambient
+    vec3 ambient = light.ambientCoefficient * surfaceColor.rgb *
+        light.intensities;
+
+    //diffuse
+    float diffuseCoefficient = max(0.0, dot(normal, surfaceToLight));
+    vec3 diffuse = diffuseCoefficient * surfaceColor.rgb * light.intensities;
+
+    //specular
+    float specularCoefficient = 0.0;
+    if(diffuseCoefficient > 0.0) {
+        specularCoefficient = pow(max(0.0, dot(surfaceToCamera,
+            reflect(-surfaceToLight, normal))), materialShininess);
+    }
+    vec3 specular = specularCoefficient * materialSpecularColor *
+        light.intensities;
+
+    //linear color (color before gamma correction)
+    return ambient + attenuation*(diffuse + specular);
+}
+
+void main() {
+    vec3 normal = normalize(transpose(inverse(mat3(model))) * fragNormal);
+    vec3 surfacePos = vec3(model * vec4(fragVert, 1));
+    vec4 surfaceColor = vec4(materialColor, 1);//texture(materialTex, fragTexCoord);
+    vec3 surfaceToCamera = normalize(cameraPosition - surfacePos);
+
+    //combine color from all the lights
+    vec3 linearColor = vec3(0);
+    for(int i = 0; i < numLights; ++i){
+        linearColor += ApplyLight(allLights[i], surfaceColor.rgb, normal,
+            surfacePos, surfaceToCamera);
+    }
+    
+    //final color (after gamma correction)
+    vec3 gamma = vec3(1.0/2.2);
+    finalColor = vec4(pow(linearColor, gamma), surfaceColor.a);
 }
--- lib/assets/shaders/basic/vertex.glsl
+++ lib/assets/shaders/basic/vertex.glsl
@@ -1,20 +1,37 @@
+/* Copyright 2012 Thomas Dalling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
 #version 150 core
 
-in vec3 position;
-in vec4 color;
-in vec2 texcoords;
-
-out vec3 f_position;
-out vec4 f_color;
-out vec2 f_texcoords;
-
-uniform mat4 world;
+uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main()
-{
-    f_texcoords = texcoords;
-    f_color = color;
-    gl_Position = projection * view * world * vec4(position, 1);
+in vec3 vert;
+in vec2 vertTexCoord;
+in vec3 vertNormal;
+
+out vec3 fragVert;
+out vec2 fragTexCoord;
+out vec3 fragNormal;
+
+void main() {
+    // Pass some variables to the fragment shader
+    fragTexCoord = vertTexCoord;
+    fragNormal = vertNormal;
+    fragVert = vert;
+    
+    // Apply all matrix transformations to vert
+    gl_Position = projection * view * model * vec4(vert, 1);
 }

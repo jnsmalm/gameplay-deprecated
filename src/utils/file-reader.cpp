0f5682363dff235a7646b27914b1b26ae87535c8
Jens Malmborg <jnsmalm@gmail.com>
2015-11-01 16:52:38 +0100

    Removed name from license.

--- src/utils/file-reader.cpp
+++ src/utils/file-reader.cpp
@@ -1,6 +1,6 @@
 /*The MIT License (MIT)
 
-JSPlay Copyright (c) 2015 Jens Malmborg
+Copyright (c) 2015 Jens Malmborg
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal

66c9f8ad979e3752e5e0c0d1eff7ca7e07c85a1e
Jens Malmborg <jnsmalm@gmail.com>
2016-01-03 21:33:27 +0100

    Added some comments.

--- examples/sprite.js
+++ examples/sprite.js
@@ -3,13 +3,17 @@ var Entity = require('../lib/entity.js').Entity;
 var Scene = require('../lib/scene.js').Scene;
 var SpriteComponent = require('../lib/sprite.js').SpriteComponent;
 
+// The game creates a window and enters a game loop which calls update and draw
+// at specified intervals.
 var game = new Game();
 var scene = new Scene(game.window);
 
+// Create the entity and add a sprite component for drawing a sprite.
 var entity = new Entity();
 entity.addComponent(new SpriteComponent({
     spriteBatch: scene.spriteBatch,
     texture: '/assets/cow.png',
+    // Pixels per unit controls the size of the sprite.
     pixelsPerUnit: 200
 }));
 

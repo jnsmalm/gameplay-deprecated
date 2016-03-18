e4fab1b6d20fdc02e5430a4c8e88b276cd33a5ca
Jens Malmborg <jnsmalm@gmail.com>
2016-03-18 20:18:03 +0100

    Added BasicShader.

--- lib/assets/shaders/basic/fragment.glsl
+++ lib/assets/shaders/basic/fragment.glsl
@@ -1,106 +1,43 @@
-/* Copyright 2012 Thomas Dalling
+#version 330 core
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+out vec4 color;
 
-   http://www.apache.org/licenses/LICENSE-2.0
+in VS_OUT {
+  vec3 position;
+  vec3 normal;
+  vec2 texCoords;
+} fs_in;
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.*/
+struct Material {
+  sampler2D diffuseMap;
+  int enableDiffuseMap;
+  vec3 diffuse;
+}; 
 
-#version 150
+struct Light {
+  vec3 direction;
+  vec3 ambient;
+  vec3 diffuse;
+};
 
-uniform mat4 model;
-uniform vec3 cameraPosition;
+uniform Material material;
+uniform Light light;
 
-uniform float materialShininess;
-uniform vec3 materialSpecularColor;
-uniform vec3 materialColor;
-uniform sampler2D materialTexture;
-uniform float materialTextureEnabled;
+void main()
+{
+  vec3 surface = material.diffuse;
+  if (material.enableDiffuseMap == 1) {
+    surface = texture(material.diffuseMap, fs_in.texCoords).rgb;
+  }
 
-#define MAX_LIGHTS 10
-uniform int numLights;
-uniform struct Light {
-   vec4 position;
-   vec3 intensities; //a.k.a the color of the light
-   float attenuation;
-   float ambientCoefficient;
-   float coneAngle;
-   vec3 coneDirection;
-} allLights[MAX_LIGHTS];
+  // Ambient
+  vec3 ambient = light.ambient * surface;
 
-in vec2 fragTexCoord;
-in vec3 fragNormal;
-in vec3 fragVert;
+  // Diffuse 
+  vec3 normal = normalize(fs_in.normal);
+  vec3 lightDir = normalize(-light.direction);  
+  float diff = max(dot(normal, lightDir), 0.0);
+  vec3 diffuse = light.diffuse * diff * surface;
 
-out vec4 finalColor;
-
-vec3 ApplyLight(Light light, vec3 surfaceColor, vec3 normal, vec3 surfacePos,
-    vec3 surfaceToCamera) {
-
-    vec3 surfaceToLight;
-    float attenuation = 1.0;
-    if(light.position.w == 0.0) {
-        //directional light
-        surfaceToLight = normalize(light.position.xyz);
-        attenuation = 1.0; //no attenuation for directional lights
-    } else {
-        //point light
-        surfaceToLight = normalize(light.position.xyz - surfacePos);
-        float distanceToLight = length(light.position.xyz - surfacePos);
-        attenuation = 1.0 / (1.0 + light.attenuation * pow(distanceToLight, 2));
-
-        //cone restrictions (affects attenuation)
-        float lightToSurfaceAngle = degrees(acos(dot(-surfaceToLight,
-            normalize(light.coneDirection))));
-        if(lightToSurfaceAngle > light.coneAngle){
-            attenuation = 0.0;
-        }
-    }
-
-    //ambient
-    vec3 ambient = light.ambientCoefficient * surfaceColor.rgb *
-        light.intensities;
-
-    //diffuse
-    float diffuseCoefficient = max(0.0, dot(normal, surfaceToLight));
-    vec3 diffuse = diffuseCoefficient * surfaceColor.rgb * light.intensities;
-
-    //specular
-    float specularCoefficient = 0.0;
-    if(diffuseCoefficient > 0.0) {
-        specularCoefficient = pow(max(0.0, dot(surfaceToCamera,
-            reflect(-surfaceToLight, normal))), materialShininess);
-    }
-    vec3 specular = specularCoefficient * materialSpecularColor *
-        light.intensities;
-
-    //linear color (color before gamma correction)
-    return ambient + attenuation*(diffuse + specular);
-}
-
-void main() {
-    vec3 normal = normalize(transpose(inverse(mat3(model))) * fragNormal);
-    vec3 surfacePos = vec3(model * vec4(fragVert, 1));
-    vec4 surfaceColor = vec4(materialColor, 1);
-    if (materialTextureEnabled != 0) {
-        surfaceColor = texture(materialTexture, fragTexCoord);
-    }
-    vec3 surfaceToCamera = normalize(cameraPosition - surfacePos);
-
-    //combine color from all the lights
-    vec3 linearColor = vec3(0);
-    for(int i = 0; i < numLights; ++i){
-        linearColor += ApplyLight(allLights[i], surfaceColor.rgb, normal,
-            surfacePos, surfaceToCamera);
-    }
-    
-    //final color (after gamma correction)
-    vec3 gamma = vec3(1.0/2.2);
-    finalColor = vec4(pow(linearColor, gamma), surfaceColor.a);
+  color = vec4(ambient + diffuse, 1.0f);
 }
--- lib/assets/shaders/basic/vertex.glsl
+++ lib/assets/shaders/basic/vertex.glsl
@@ -1,37 +1,22 @@
-/* Copyright 2012 Thomas Dalling
+#version 330 core
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 texCoords;
 
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.*/
-
-#version 150 core
+out VS_OUT {
+  vec3 position;
+  vec3 normal;
+  vec2 texCoords;
+} vs_out;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-in vec3 vert;
-in vec2 vertTexCoord;
-in vec3 vertNormal;
-
-out vec3 fragVert;
-out vec2 fragTexCoord;
-out vec3 fragNormal;
-
 void main() {
-    // Pass some variables to the fragment shader
-    fragTexCoord = vertTexCoord;
-    fragNormal = vertNormal;
-    fragVert = vert;
-    
-    // Apply all matrix transformations to vert
-    gl_Position = projection * view * model * vec4(vert, 1);
+  gl_Position = projection * view * model * vec4(position, 1.0f);
+  vs_out.position = vec3(model * vec4(position, 1.0f));
+  vs_out.normal = mat3(transpose(inverse(model))) * normal;
+  vs_out.texCoords = texCoords;
 }

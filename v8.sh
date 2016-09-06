23d6697229e17640f03f7ea89be626b2eb36236c
Jens Malmborg <jnsmalm@gmail.com>
2016-09-06 22:35:16 +0200

    Added bash script to fetch and build V8.

---
+++ v8.sh
@@ -0,0 +1,10 @@
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=`pwd`/depot_tools:"$PATH"
+gclient
+cd deps
+fetch v8
+cd v8
+git checkout tags/4.9.385.33
+gclient sync --with_branch_heads --jobs 16
+sed -i '' "s/'CLANG_CXX_LANGUAGE_STANDARD': 'gnu++0x'/& ,'CLANG_CXX_LIBRARY': 'libc++'/" build/standalone.gypi
+make native

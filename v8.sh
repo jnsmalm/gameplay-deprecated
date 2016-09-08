242aeb846ac89a10a862edffc6b2bd5e126237fa
Jens Malmborg <jnsmalm@gmail.com>
2016-09-08 21:25:09 +0200

    Disable tests when building V8.

--- v8.sh
+++ v8.sh
@@ -7,4 +7,6 @@ cd v8
 git checkout tags/4.9.385.33
 gclient sync --with_branch_heads --jobs 16
 sed -i '' "s/'CLANG_CXX_LANGUAGE_STANDARD': 'gnu++0x'/& ,'CLANG_CXX_LIBRARY': 'libc++'/" build/standalone.gypi
+sed -i '' "/cctest.gyp/d" build/all.gyp
+sed -i '' "/unittests.gyp/d" build/all.gyp
 make native
